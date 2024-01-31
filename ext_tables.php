d820d7b816c95db4966848f1446fb67c77c413ec
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-01-31 08:32:59 +0100

    Update ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -6,27 +6,27 @@ call_user_func(
 	{
 		
 		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-			'tx_verowaconnect_domain_model_event',
-			'EXT:verowa_connect/Resources/Private/Language/locallang_csh_tx_verowaconnect_domain_model_event.xlf'
+			'tx_verowaconnector_domain_model_event',
+			'EXT:verowa_connector/Resources/Private/Language/locallang_csh_tx_verowaconnector_domain_model_event.xlf'
 		);
 		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
-			'tx_verowaconnect_domain_model_event'
+			'tx_verowaconnector_domain_model_event'
 		);
 		
 		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-			'tx_verowaconnect_domain_model_organizer',
-			'EXT:verowa_connect/Resources/Private/Language/locallang_csh_tx_verowaconnect_domain_model_organizer.xlf'
+			'tx_verowaconnector_domain_model_organizer',
+			'EXT:verowa_connector/Resources/Private/Language/locallang_csh_tx_verowaconnector_domain_model_organizer.xlf'
 		);
 		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
-			'tx_verowaconnect_domain_model_organizer'
+			'tx_verowaconnector_domain_model_organizer'
 		);
 		
 		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-			'tx_verowaconnect_domain_model_room',
-			'EXT:verowa_connect/Resources/Private/Language/locallang_csh_tx_verowaconnect_domain_model_room.xlf'
+			'tx_verowaconnector_domain_model_room',
+			'EXT:verowa_connector/Resources/Private/Language/locallang_csh_tx_verowaconnector_domain_model_room.xlf'
 		);
 		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
-			'tx_verowaconnect_domain_model_room'
+			'tx_verowaconnector_domain_model_room'
 		);
 		
 	}
