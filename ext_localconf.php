203ff9355ebf242945c784b0e18fa4c027bf723b
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-01-31 08:35:29 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,41 +5,41 @@ call_user_func(
 	static function()
 	{
 		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-			'VerowaConnect',
+			'VerowaConnector',
 			'Eventlist',
 			[
-				\Checkitsedo\VerowaConnect\Controller\EventController::class => 'list'
+				\Checkitsedo\VerowaConnector\Controller\EventController::class => 'list'
 			],
 			// non-cacheable actions
 			[
-				\Checkitsedo\VerowaConnect\Controller\EventController::class => 'list',
-				\Checkitsedo\VerowaConnect\Controller\OrganizerController::class => '',
-				\Checkitsedo\VerowaConnect\Controller\RoomController::class => ''
+				\Checkitsedo\VerowaConnector\Controller\EventController::class => 'list',
+				\Checkitsedo\VerowaConnector\Controller\OrganizerController::class => '',
+				\Checkitsedo\VerowaConnector\Controller\RoomController::class => ''
 			]
 		);
 		
 		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-			'VerowaConnect',
+			'VerowaConnector',
 			'Eventdetails',
 			[
-				\Checkitsedo\VerowaConnect\Controller\EventController::class => 'show'
+				\Checkitsedo\VerowaConnector\Controller\EventController::class => 'show'
 			],
 			// non-cacheable actions
 			[
-				\Checkitsedo\VerowaConnect\Controller\EventController::class => '',
-				\Checkitsedo\VerowaConnect\Controller\OrganizerController::class => '',
-				\Checkitsedo\VerowaConnect\Controller\RoomController::class => ''
+				\Checkitsedo\VerowaConnector\Controller\EventController::class => '',
+				\Checkitsedo\VerowaConnector\Controller\OrganizerController::class => '',
+				\Checkitsedo\VerowaConnector\Controller\RoomController::class => ''
 			]
 		);
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:verowa_connect/Configuration/TsConfig/Page/Mod/Wizards/NewContentElement.tsconfig">'
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:verowa_connector/Configuration/TsConfig/Page/Mod/Wizards/NewContentElement.tsconfig">'
 );
 
 		
 		$icons = [
-            'verowa_connect-plugin-eventlist' => 'EXT:verowa_connect/Resources/Public/Icons/user_plugin_eventlist.svg',
-			'verowa_connect-plugin-eventdetails' => 'EXT:verowa_connect/Resources/Public/Icons/user_plugin_eventdetails.svg',
+            'verowa_connector-plugin-eventlist' => 'EXT:verowa_connector/Resources/Public/Icons/user_plugin_eventlist.svg',
+			'verowa_connector-plugin-eventdetails' => 'EXT:verowa_connector/Resources/Public/Icons/user_plugin_eventdetails.svg',
         ];
 		
 		$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
@@ -57,21 +57,21 @@ call_user_func(
 				wizards.newContentElement.wizardItems.plugins {
 					elements {
 						eventlist {
-							iconIdentifier = verowa_connect-plugin-eventlist
-							title = LLL:EXT:verowa_connect/Resources/Private/Language/locallang_db.xlf:tx_verowa_connect_eventlist.name
-							description = LLL:EXT:verowa_connect/Resources/Private/Language/locallang_db.xlf:tx_verowa_connect_eventlist.description
+							iconIdentifier = verowa_connector-plugin-eventlist
+							title = LLL:EXT:verowa_connector/Resources/Private/Language/locallang_db.xlf:tx_verowa_connector_eventlist.name
+							description = LLL:EXT:verowa_connector/Resources/Private/Language/locallang_db.xlf:tx_verowa_connector_eventlist.description
 							tt_content_defValues {
 								CType = list
-								list_type = verowaconnect_eventlist
+								list_type = verowaconnector_eventlist
 							}
 						}
 						eventdetails {
-							iconIdentifier = verowa_connect-plugin-eventdetails
-							title = LLL:EXT:verowa_connect/Resources/Private/Language/locallang_db.xlf:tx_verowa_connect_eventdetails.name
-							description = LLL:EXT:verowa_connect/Resources/Private/Language/locallang_db.xlf:tx_verowa_connect_eventdetails.description
+							iconIdentifier = verowa_connector-plugin-eventdetails
+							title = LLL:EXT:verowa_connector/Resources/Private/Language/locallang_db.xlf:tx_verowa_connector_eventdetails.name
+							description = LLL:EXT:verowa_connector/Resources/Private/Language/locallang_db.xlf:tx_verowa_connector_eventdetails.description
 							tt_content_defValues {
 								CType = list
-								list_type = verowaconnect_eventdetails
+								list_type = verowaconnector_eventdetails
 							}
 						}
 					}
