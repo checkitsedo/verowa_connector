53d4524a069e8f858f8f3e976dee1689af3f67cd
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-02-05 16:52:58 +0100

    Update EventController.php

--- Classes/Controller/EventController.php
+++ Classes/Controller/EventController.php
@@ -27,57 +27,57 @@ use TYPO3\CMS\Extbase\Pagination\QueryResultPaginator;
 class EventController extends ActionController
 {
 
-    /**
-     * eventRepository
-     *
-     * @var \Checkitsedo\VerowaConnector\Domain\Repository\EventRepository
-     */
-    protected $eventRepository = null;
+	/**
+ 	 * eventRepository
+   	 *
+     	 * @var \Checkitsedo\VerowaConnector\Domain\Repository\EventRepository
+       	 */
+	protected $eventRepository = null;
 
-    /**
-     * @param \Checkitsedo\VerowaConnector\Domain\Repository\EventRepository $eventRepository
-     */
-    public function injectEventRepository(\Checkitsedo\VerowaConnector\Domain\Repository\EventRepository $eventRepository)
-    {
-        $this->eventRepository = $eventRepository;
-    }
+	/**
+ 	 * @param \Checkitsedo\VerowaConnector\Domain\Repository\EventRepository $eventRepository
+   	 */
+	public function injectEventRepository(\Checkitsedo\VerowaConnector\Domain\Repository\EventRepository $eventRepository)
+	{
+		$this->eventRepository = $eventRepository;
+	}
 
-    /**
-     * action list
-     *
-     * @return string|object|null|void
-     */
-    public function listAction()
-    {
+	/**
+ 	 * action list
+   	 *
+     	 * @return string|object|null|void
+       	 */
+	public function listAction()
+	{
 		$this->cacheService->clearPageCache($pid);
 
-        $events = $this->eventRepository->findAll();
-		
+		$events = $this->eventRepository->findAll();
+
 		// pagination
 		$itemsPerPage = 10;
 		$maximumLinks = 5;
 		$currentPage = $this->request->hasArgument('currentPage') ? (int)$this->request->getArgument('currentPage') : 1;
 		$paginator = new \TYPO3\CMS\Extbase\Pagination\QueryResultPaginator($events, $currentPage, $itemsPerPage);
 		$pagination = new \GeorgRinger\NumberedPagination\NumberedPagination($paginator, $maximumLinks);
-		
+
 		$this->view->assign('pagination', [
 			'paginator' => $paginator,
 			'pagination' => $pagination,
 		]);
-    }
+	}
 
-    /**
-     * action show
-     *
-     * @param \Checkitsedo\VerowaConnector\Domain\Model\Event $event
-     * @return string|object|null|void
-     */
-    public function showAction(\Checkitsedo\VerowaConnector\Domain\Model\Event $event)
-    {
-        $this->view->assign('event', $event);
-    }
+	/**
+ 	 * action show
+   	 *
+     	 * @param \Checkitsedo\VerowaConnector\Domain\Model\Event $event
+       	 * @return string|object|null|void
+	 */
+	public function showAction(\Checkitsedo\VerowaConnector\Domain\Model\Event $event)
+	{
+		$this->view->assign('event', $event);
+	}
 	protected function getErrorFlashMessage()
-    {
-        return false;
-    }
+	{
+		return false;
+	}
 }
