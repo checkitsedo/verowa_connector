ffe4babdd1c642a9176085bf40d845ae7c40838d
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-02-05 22:55:26 +0100

    Update tt_content.php

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,27 +2,27 @@
 defined('TYPO3_MODE') || die();
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'VerowaConnect',
+    'VerowaConnector',
     'Eventlist',
-    'LLL:EXT:verowa_connect/Resources/Private/Language/locallang.xlf:plugin.eventlist.title',
-	'EXT:verowa_connect/Resources/Public/Icons/user_plugin_eventlist.svg'
+    'LLL:EXT:verowa_connector/Resources/Private/Language/locallang.xlf:plugin.eventlist.title',
+	'EXT:verowa_connector/Resources/Public/Icons/user_plugin_eventlist.svg'
 );
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'VerowaConnect',
+    'VerowaConnector',
     'Eventdetails',
-    'LLL:EXT:verowa_connect/Resources/Private/Language/locallang.xlf:plugin.eventdetails.title',
-	'EXT:verowa_connect/Resources/Public/Icons/user_plugin_eventdetails.svg'
+    'LLL:EXT:verowa_connector/Resources/Private/Language/locallang.xlf:plugin.eventdetails.title',
+	'EXT:verowa_connector/Resources/Public/Icons/user_plugin_eventdetails.svg'
 );
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['verowaconnect_eventlist'] = 'pi_flexform';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['verowaconnector_eventlist'] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    'verowaconnect_eventlist',
-    'FILE:EXT:verowa_connect/Configuration/FlexForms/PluginEventlist.xml'
+    'verowaconnector_eventlist',
+    'FILE:EXT:verowa_connector/Configuration/FlexForms/PluginEventlist.xml'
 );
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['verowaconnect_eventdetails'] = 'pi_flexform';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['verowaconnector_eventdetails'] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    'verowaconnect_eventdetails',
-    'FILE:EXT:verowa_connect/Configuration/FlexForms/PluginEventdetails.xml'
+    'verowaconnector_eventdetails',
+    'FILE:EXT:verowa_connector/Configuration/FlexForms/PluginEventdetails.xml'
 );
