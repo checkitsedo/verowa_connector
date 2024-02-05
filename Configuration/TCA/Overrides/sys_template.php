75e4d40ea8f38f3d1c499c1ea2c4409a1d372b4b
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-02-05 22:54:17 +0100

    Update sys_template.php

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,4 +1,4 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('verowa_connect', 'Configuration/TypoScript', 'Verowa Connect');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('verowa_connector', 'Configuration/TypoScript', 'Verowa Connector');
