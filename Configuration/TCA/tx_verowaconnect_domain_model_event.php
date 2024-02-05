68e9b6ace2a2ce5c668288e708a1bcd26e05a285
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-02-05 17:36:35 +0100

    Update tx_verowaconnect_domain_model_event.php

--- Configuration/TCA/tx_verowaconnect_domain_model_event.php
+++ Configuration/TCA/tx_verowaconnect_domain_model_event.php
@@ -1,31 +1,31 @@
 <?php
 return [
-    'ctrl' => [
-        'title' => 'LLL:EXT:verowa_connect/Resources/Private/Language/locallang_db.xlf:tx_verowaconnect_domain_model_event',
-        'label' => 'event_id',
-        'tstamp' => 'tstamp',
-        'crdate' => 'crdate',
-        'cruser_id' => 'cruser_id',
-        'sortby' => 'sorting',
-        'versioningWS' => true,
-        'languageField' => 'sys_language_uid',
-        'transOrigPointerField' => 'l10n_parent',
-        'transOrigDiffSourceField' => 'l10n_diffsource',
-        'delete' => 'deleted',
-        'enablecolumns' => [
-            'disabled' => 'hidden',
-            'starttime' => 'starttime',
-            'endtime' => 'endtime',
-        ],
-        'searchFields' => 'event_id,title,short_desc,long_desc,childcare_id,subscribe_person_id,baptism_offer_id,event_state,date_text,image_url,room_id,room_name,room_public_name,room_location_name,room_location_url,room_location_url_is_external',
-        'iconfile' => 'EXT:verowa_connect/Resources/Public/Icons/tx_verowaconnect_domain_model_event.gif'
-    ],
+	'ctrl' => [
+		'title' => 'LLL:EXT:verowa_connector/Resources/Private/Language/locallang_db.xlf:tx_verowaconnector_domain_model_event',
+		'label' => 'event_id',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'sortby' => 'sorting',
+		'versioningWS' => true,
+	        'languageField' => 'sys_language_uid',
+	        'transOrigPointerField' => 'l10n_parent',
+	        'transOrigDiffSourceField' => 'l10n_diffsource',
+	        'delete' => 'deleted',
+	        'enablecolumns' => [
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
+			'endtime' => 'endtime',
+		],
+		'searchFields' => 'event_id,date_text,title,topic,short_desc,long_desc,subs_person_id,collection,target_groups,layers,rooms,image_url',
+		'iconfile' => 'EXT:verowa_connector/Resources/Public/Icons/tx_verowaconnector_domain_model_event.gif'
+	],
 	'external' => [
 		'general' => [
 			0 => [
 				'connector' => 'json',
 				'parameters' => [
-					'uri' => 'https://api.verowa.ch/getevents/stjosef-zuerich/1ad89c27e1a89ef6aa34f34e4adf7448',
+					'uri' => 'https://api.verowa.ch/geteventdetails/stjosef-zuerich/1ad89c27e1a89ef6aa34f34e4adf7448/0',
 					'encoding' => 'utf-8',
 					'headers' => [
 						'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0',
@@ -41,92 +41,93 @@ return [
 			]
 		]
 	],
-    'types' => [
-        '1' => ['showitem' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, event_id, date_from, date_to, title, hide_time, short_desc, long_desc, childcare_id, subscribe_date, subscribe_time, subscribe_person_id, baptism_offer_id, event_type, event_state, date_text, image_url, subscriptions, organizer, rooms, room_id, room_name, room_public_name, room_location_name, room_location_url, room_location_url_is_external, --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access, starttime, endtime'],
-    ],
-    'columns' => [
-        'sys_language_uid' => [
-            'exclude' => true,
-            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.language',
-            'config' => [
-                'type' => 'select',
-                'renderType' => 'selectSingle',
-                'special' => 'languages',
-                'items' => [
-                    [
-                        'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.allLanguages',
-                        -1,
-                        'flags-multiple'
-                    ]
-                ],
-                'default' => 0,
-            ],
-        ],
-        'l10n_parent' => [
-            'displayCond' => 'FIELD:sys_language_uid:>:0',
-            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.l18n_parent',
-            'config' => [
-                'type' => 'select',
-                'renderType' => 'selectSingle',
-                'default' => 0,
-                'items' => [
-                    ['', 0],
-                ],
-                'foreign_table' => 'tx_verowaconnect_domain_model_event',
-                'foreign_table_where' => 'AND {#tx_verowaconnect_domain_model_event}.{#pid}=###CURRENT_PID### AND {#tx_verowaconnect_domain_model_event}.{#sys_language_uid} IN (-1,0)',
-            ],
-        ],
-        'l10n_diffsource' => [
-            'config' => [
-                'type' => 'passthrough',
-            ],
-        ],
-        'hidden' => [
-            'exclude' => true,
-            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.visible',
-            'config' => [
-                'type' => 'check',
-                'renderType' => 'checkboxToggle',
-                'items' => [
-                    [
-                        0 => '',
-                        1 => '',
-                        'invertStateDisplay' => true
-                    ]
-                ],
-            ],
-        ],
-        'starttime' => [
-            'exclude' => true,
-            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.starttime',
-            'config' => [
-                'type' => 'input',
-                'renderType' => 'inputDateTime',
-                'eval' => 'datetime,int',
-                'default' => 0,
-                'behaviour' => [
-                    'allowLanguageSynchronization' => true
-                ]
-            ],
-        ],
-        'endtime' => [
-            'exclude' => true,
-            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.endtime',
-            'config' => [
-                'type' => 'input',
-                'renderType' => 'inputDateTime',
-                'eval' => 'datetime,int',
-                'default' => 0,
-                'range' => [
-                    'upper' => mktime(0, 0, 0, 1, 1, 2038)
-                ],
-                'behaviour' => [
-                    'allowLanguageSynchronization' => true
-                ]
-            ],
-        ],
+	'types' => [
+		'1' => ['showitem' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, event_id, date_from, date_to, date_text, hide_time, title, topic, short_desc, long_desc, organizer, subs_date, subs_time, subs_person_id, collection, target_groups, layers, rooms, --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access, starttime, endtime'],
+	],
+	'columns' => [
+		'sys_language_uid' => [
+			'exclude' => true,
+			'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.language',
+			'config' => [
+				'type' => 'select',
+				'renderType' => 'selectSingle',
+				'special' => 'languages',
+				'items' => [
+					[
+						'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.allLanguages',
+						-1,
+						'flags-multiple'
+					]
+				],
+				'default' => 0,
+			],
+		],
+		'l10n_parent' => [
+			'displayCond' => 'FIELD:sys_language_uid:>:0',
+			'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.l18n_parent',
+			'config' => [
+				'type' => 'select',
+				'renderType' => 'selectSingle',
+				'default' => 0,
+				'items' => [
+					['', 0],
+				],
+				'foreign_table' => 'tx_verowaconnector_domain_model_event',
+				'foreign_table_where' => 'AND {#tx_verowaconnector_domain_model_event}.{#pid}=###CURRENT_PID### AND {#tx_verowaconnector_domain_model_event}.{#sys_language_uid} IN (-1,0)',
+			],
+		],
+		'l10n_diffsource' => [
+			'config' => [
+				'type' => 'passthrough',
+			],
+		],
+		'hidden' => [
+			'exclude' => true,
+			'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.visible',
+			'config' => [
+				'type' => 'check',
+				'renderType' => 'checkboxToggle',
+				'items' => [
+					[
+						0 => '',
+						1 => '',
+						'invertStateDisplay' => true
+					]
+				],
+			],
+		],
+		'starttime' => [
+			'exclude' => true,
+			'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.starttime',
+			'config' => [
+				'type' => 'input',
+				'renderType' => 'inputDateTime',
+				'eval' => 'datetime,int',
+				'default' => 0,
+				'behaviour' => [
+					'allowLanguageSynchronization' => true
+				]
+			],
+		],
+		'endtime' => [
+			'exclude' => true,
+			'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.endtime',
+			'config' => [
+				'type' => 'input',
+				'renderType' => 'inputDateTime',
+				'eval' => 'datetime,int',
+				'default' => 0,
+				'range' => [
+					'upper' => mktime(0, 0, 0, 1, 1, 2038)
+				],
+				'behaviour' => [
+					'allowLanguageSynchronization' => true
+				]
+			],
+		],
 
-        'event_id' => [
+		'event_id' => [
+	
             'exclude' => true,
             'label' => 'LLL:EXT:verowa_connect/Resources/Private/Language/locallang_db.xlf:tx_verowaconnect_domain_model_event.event_id',
             'config' => [
